588f44a1f42eca22175b194a24a64fcff264d28c
Jason <quietjason@gmail.com>
2016-05-13 15:35:13 +0800

    Add empty project of GoogleAnalyticsSwiftDemo.

---
+++ source/GoogleAnalyticsSwiftDemo/GoogleAnalyticsSwiftDemo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  GoogleAnalyticsSwiftDemo
+//
+//  Created by Jason Zheng on 5/13/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  var mainWindowController: MainWindowController?
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+      
+    let mainWindowController = MainWindowController()
+    mainWindowController.showWindow(self)
+    
+    self.mainWindowController = mainWindowController
+  }
+}
+
---
+++ source/GoogleAnalyticsSwiftDemo/GoogleAnalyticsSwiftDemo/MainWindowController.swift
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.swift
+//  GoogleAnalyticsSwiftDemo
+//
+//  Created by Jason Zheng on 5/13/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
+
